a6cd131cacda5a12f9a8650fd3a5fc0c1bf82382
Guillermo Jackson <gijcancer@gmail.com>
2023-07-29 19:58:18 -0400

    New JS, removed some stuff from index.html

--- scripts.js
+++ scripts.js
@@ -17,93 +17,108 @@ document.addEventListener('DOMContentLoaded', function() {
     hideListItems();
 });
 
-var removedButton = [];
-var removedButtons = [];
+const userQuestionsArray = [ "How old are you?", "User question two.", "User question three.", "User question four.", "User question five." ]
+
+let removedButton = [];
+let removedButtons = [];
 
 function removeAllButtonsWithClass(className) {
-    var buttons = document.querySelectorAll('.' + className);
+    let buttons = document.querySelectorAll('.' + className);
     buttons.forEach(function(button) {
         removedButtons.push(button.textContent);
         button.remove();
     });
+    observeNewButtons()
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var fadeButtons = document.querySelectorAll('.fadeButton');
+function generateUserQuestions() {
 
-    fadeButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            button.classList.add('hidden');
-            removedButton.push(button.textContent);
-            removeAllButtonsWithClass('fadeButton')
-            var undoButton = createAndAppendElementToSection('UserQuestions', 'button', 'Undo');
-            undoButton.className = 'EmptySpace';
-            removedButtons.splice(removedButtons.indexOf(removedButton[0]), 1);
-        });
-    });
-});
+    for(let i = 0; i < userQuestionsArray.length; i++) {
 
-const buttonContainer = document.getElementById('UserQuestions');
-  
-buttonContainer.addEventListener('click', handleButtonClick);
-  
+        let fadeButton = createAndAppendElementToSection ('UserQuestions', 'button', userQuestionsArray[i]);
+        fadeButton.className = 'fadeButton';
+        fadeButton.addEventListener('click', handleButtonClick);
+    }
+    observeNewButtons()
+}
+
+document.addEventListener('DOMContentLoaded', generateUserQuestions)
+
+function fadeTheFadeButtons (targetButton) {
+    targetButton.classList.add('hidden');
+    setTimeout(function() {
+        var emptySpace = document.createElement('button');
+        emptySpace.className = 'EmptySpace';
+        var textNode = document.createTextNode('Undo');
+        emptySpace.appendChild(textNode);
+        removedButton.push(targetButton.textContent);
+        targetButton.parentNode.insertBefore(emptySpace, targetButton);
+        removeAllButtonsWithClass("fadeButton");
+    }, 1000);
+    observeNewButtons();
+};
+
+function createAndAppendElementToSection(sectionId, elementType, content) {
+    const section = document.getElementById(sectionId);
+
+    if (section) {
+        const newElement = document.createElement(elementType);
+
+        if (content) {
+        newElement.textContent = content;
+        }
+
+        section.appendChild(newElement);
+
+        observeNewButtons()
+        return newElement;
+    } 
+    else {
+        console.error(`Section element with ID '${sectionId}' not found.`);
+        return null;
+    }
+};
+
 function handleButtonClick(event) {
+    
     const targetButton = event.target;
 
     if (targetButton.className === 'EmptySpace') {
-
-        var clickedButton = createAndAppendElementToSection('UserQuestions','button', removedButton[0]);
-
-        clickedButton.className = 'fadeButton';
-
-        for(let i=0; i < removedButtons.length; i++){
-            var undoButtons = createAndAppendElementToSection('UserQuestions','button', removedButtons[i]);
-            undoButtons.className = 'fadeButton';
-            //need to do something here that retains the order of the buttons because as of right now it puts the removed button at the top and the rest of the buttons
-        }
-        targetButton.remove();
-        removedButton = [];
-        removedButtons = [];
+        
+        targetButton.classList.add('hidden');
+        setTimeout(function() {
+            for(let i=0; i < removedButtons.length; i++){
+                var undoButtons = createAndAppendElementToSection('UserQuestions','button', removedButtons[i]);
+                undoButtons.className = 'fadeButton';};
+            targetButton.remove();
+            removeAllButtonsWithClass("ClickedButton");
+            removedButton = [];
+            removedButtons = [];},
+            1000);
+        observeNewButtons();
     }
 
     if (targetButton.className === 'fadeButton')
     {
-        var fadeButtons = document.querySelectorAll('.fadeButton');
+        fadeTheFadeButtons(targetButton);
+    };
+};
 
-        fadeButtons.forEach(function(button) {
-            button.addEventListener('click', function() {
-                button.classList.add('hidden');
+function observeNewButtons() {
 
-                setTimeout(function() {
-                    var emptySpace = document.createElement('button');
-                    emptySpace.className = 'EmptySpace';
-                    var textNode = document.createTextNode('Undo');
-                    emptySpace.appendChild(textNode);
-                    removedButton.push(button.textContent);
-                    button.parentNode.insertBefore(emptySpace, button);
-                    removeAllButtonsWithClass("fadeButton");
-                    removedButtons.splice(removedButtons.indexOf(removedButton[0]), 1);
-                }, 1000);
-            });
+const buttonContainer = document.getElementById('UserQuestions');
+
+const observer = new MutationObserver(function(mutationsList, observer) {
+    for (let mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+        mutation.addedNodes.forEach(function(node) {
+        if (node.tagName === 'BUTTON') {
+            node.addEventListener('click', handleButtonClick);
+        }
         });
     }
-  };
-
-  function createAndAppendElementToSection(sectionId, elementType, content) {
-    const section = document.getElementById(sectionId);
-  
-    if (section) {
-      const newElement = document.createElement(elementType);
-
-      if (content) {
-        newElement.textContent = content;
-      }
-  
-      section.appendChild(newElement);
-  
-      return newElement;
-    } else {
-      console.error(`Section element with ID '${sectionId}' not found.`);
-      return null;
     }
-  }
+});
+observer.observe(buttonContainer, { childList: true });
+};
+  
